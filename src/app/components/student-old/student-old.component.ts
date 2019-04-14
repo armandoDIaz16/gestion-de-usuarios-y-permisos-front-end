fbc5aa01b6200aa383c8bc045d78b68730f97a9b
Miguel Peña <miguelangel.plopez@itleon.edu.mx>
2019-04-14 11:42:13 -0500

    Se agregan datos personales en "obtener contraeña"

--- src/app/components/student-old/student-old.component.ts
+++ src/app/components/student-old/student-old.component.ts
@@ -1,76 +1,88 @@
-import { Component, OnInit } from '@angular/core';
-import { JarwisService } from '../../services/jarwis.service';
-import { TokenService } from '../../services/token.service';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {JarwisService} from '../../services/jarwis.service';
+import {TokenService} from '../../services/token.service';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-dashboard',  
-  templateUrl: './student-old.component.html',
-  styleUrls: ['./student-old.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './student-old.component.html',
+    styleUrls: ['./student-old.component.scss']
 })
 export class StudentOldComponent implements OnInit {
 
-  public form ={
-   email:null,
-    //name:null,
-    name:localStorage.getItem("nombre"),
-    password:null,
-    password_confirmation:null,
-    curp: null,
-     //PRIMER_APELLIDO:'chavez',
-     //SEGUNDO_APELLIDO:'barajas',
-   /*  FECHA_NACIMIENTO:'2019-01-11',
-     CURP: 'cabe960224hgthrd02',
-     ESTADO:1,
-     TELEFONO_CASA: '4771234567',
-     TELEFONO_MOVIL: '4771234567',
-     CORREO1:'14240683@itleon.edu.mx' ,
-     CORREO2: '14240683@itleon.edu.mx' ,
-     CORREO_INSTITUCIONAL: '14240683@itleon.edu.mx' ,
-     //CONTRASENIA:'123' ,
-     CALLE:'lomas',
-     NUMERO_EXTERIOR: '12',
-     NUMERO_INTERIOR: '2014',
-     NACIONALIDAD: 'mexicana',
-     SEXO: 'm',
-     TIPO_SANGUINEO: 'o+',
-     NSS: '1222114',
-     NOMBRE_CONTACTO: 'juan',
-     TELEFONO_CONTACTO: '4771234567',
-     CORREO_CONTACTO: '14240683@itleon.edu.mx' ,
-     FK_COLONIA:1 ,
-     FK_ESTADO_CIVIL: 1 ,
-     FK_USUARIO_REGISTRO: 1 ,
-     FECHA_REGISTRO: '2019-01-11',
-     FK_USUARIO_MODIFICACION:1 ,
-     FECHA_MODIFICACION:'2019-01-11',
-     BORRADO:'0' */
-  };
+    public get_datos_alumno(){
+        let json = JSON.parse(localStorage.getItem('datos_alumno'));
+        return json;
+    }
 
-  public error = [];
+    public datos_alumno = this.get_datos_alumno();
 
-  constructor (private Jarwis: JarwisService,
-    private Token : TokenService,
-    private router : Router) { }
+    public form = {
+        email: null,
+        //name:null,
+        numero_control:   this.datos_alumno.numero_control,
+        name:             this.datos_alumno.nombre,
+        PRIMER_APELLIDO:  this.datos_alumno.primer_apellido,
+        SEGUNDO_APELLIDO: this.datos_alumno.segundo_apellido,
+        password: null,
+        password_confirmation: null,
+        curp: null,
+        //PRIMER_APELLIDO:'chavez',
+        //SEGUNDO_APELLIDO:'barajas',
+        /*  FECHA_NACIMIENTO:'2019-01-11',
+          CURP: 'cabe960224hgthrd02',
+          ESTADO:1,
+          TELEFONO_CASA: '4771234567',
+          TELEFONO_MOVIL: '4771234567',
+          CORREO1:'14240683@itleon.edu.mx' ,
+          CORREO2: '14240683@itleon.edu.mx' ,
+          CORREO_INSTITUCIONAL: '14240683@itleon.edu.mx' ,
+          //CONTRASENIA:'123' ,
+          CALLE:'lomas',
+          NUMERO_EXTERIOR: '12',
+          NUMERO_INTERIOR: '2014',
+          NACIONALIDAD: 'mexicana',
+          SEXO: 'm',
+          TIPO_SANGUINEO: 'o+',
+          NSS: '1222114',
+          NOMBRE_CONTACTO: 'juan',
+          TELEFONO_CONTACTO: '4771234567',
+          CORREO_CONTACTO: '14240683@itleon.edu.mx' ,
+          FK_COLONIA:1 ,
+          FK_ESTADO_CIVIL: 1 ,
+          FK_USUARIO_REGISTRO: 1 ,
+          FECHA_REGISTRO: '2019-01-11',
+          FK_USUARIO_MODIFICACION:1 ,
+          FECHA_MODIFICACION:'2019-01-11',
+          BORRADO:'0' */
+    };
 
-  onSubmit(){
-    this.Jarwis.signup(this.form).subscribe(
-      data => this.handleResponse(data),
-      error => this.handleError(error)
-    );
-  }
+    public error = [];
 
-  handleResponse(data){
-    this.Token.handle(data.access_token);
-    this.router.navigateByUrl('/home');
-  }
+    constructor(private Jarwis: JarwisService,
+                private Token: TokenService,
+                private router: Router) {
+    }
+
+    onSubmit() {
+        this.Jarwis.signup(this.form).subscribe(
+            data => this.handleResponse(data),
+            error => this.handleError(error)
+        );
+    }
+
+    handleResponse(data) {
+        //this.Token.handle(data.access_token);
+        localStorage.clear();
+        this.router.navigateByUrl('/login');
+    }
 
 
-  handleError(error){
-    this.error= error.error.errors;
-  }
+    handleError(error) {
+        this.error = error.error.errors;
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -5,24 +5,24 @@ import { TokenService } from '../../services/token.service';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { UsuarioRolesService } from '../../services/usuraio-roles.service';
-//import { navItems } from './../../_nav';
-//import {showSystem} from '../../components/navbar-sistems/navbar-sistems.component';
+// import { navItems } from './../../_nav';
+// import {showSystem} from '../../components/navbar-sistems/navbar-sistems.component';
 
-let rutasSistema = []
+const rutasSistema = [];
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html',
   providers: [UsuarioRolesService]
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
 
-  //public navItems;
+  // public navItems;
   public form = {
     curp: null,
     password: null
   };
-  
+
   public error = null;
   public usuarioRolesLista = [];
   public data = null;
@@ -30,9 +30,9 @@ export class LoginComponent implements OnInit{
 
 
   constructor(private Jarwis: JarwisService,
-    private Token : TokenService,
-    private router : Router,
-    private Auth : AuthService,
+    private Token: TokenService,
+    private router: Router,
+    private Auth: AuthService,
     private usuarioRolesService: UsuarioRolesService
 
     ) {  }
@@ -44,31 +44,30 @@ export class LoginComponent implements OnInit{
     );
   }
 
-  handleResponse(data){
+  handleResponse(data) {
     this.Token.handle(data.access_token);
     this.Auth.changeAuthStatus(true);
-    sessionStorage.setItem('IdUsuario',data.IdUsuario)
-    
+    sessionStorage.setItem('IdUsuario', data.IdUsuario);
+
      this.usuarioRolesService.getUsuarioRoles().subscribe(data => {
-     sessionStorage['sistemas'] = JSON.stringify(data);    
+     sessionStorage['sistemas'] = JSON.stringify(data);
      this.router.navigateByUrl('/home');
-     //agregar el json de sistemas para empezar a comparar
-   }); 
-    
+     // agregar el json de sistemas para empezar a comparar
+   });
+
   }
 
-  handleError(error){
-    this.error= error.error.error;
+  handleError(error) {
+    this.error = error.error.error;
   }
   ngOnInit() {
-    if(sessionStorage.rutas){
+    if (sessionStorage.rutas) {
       sessionStorage.clear();
       localStorage.clear();
       console.clear();
       location.reload();
-    }   
+    }
   }
  }
 
  export const rutasSistemas = rutasSistema;
- 
