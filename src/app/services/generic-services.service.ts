87f6b16297c0d5351bf9a94e3080770cdaa45b6c
Miguel Peña <miguelangel.plopez@itleon.edu.mx>
2019-05-27 17:08:46 -0500

    Se agregan llamada a servicios genérico y configuración de URL back

---
+++ src/app/services/generic-services.service.ts
@@ -0,0 +1,50 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpRequest} from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GenericServicesService {
+
+    protected static API_ENDPOINT = 'http://127.0.0.1:8000/api/';
+    // protected static API_ENDPOINT = 'http://10.0.31.10/backend_swiitl/server.php/api/';
+
+    /*protected static HEADERS = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    };*/
+
+    protected httpClient: HttpClient;
+
+    constructor(httpClient: HttpClient) {
+        this.httpClient = httpClient;
+    }
+
+    get(ruta: string) {
+        return this.httpClient.get(
+            GenericServicesService.API_ENDPOINT + ruta
+        );
+    }
+
+    post(ruta: string, body: object) {
+        return this.httpClient.post(
+            GenericServicesService.API_ENDPOINT + ruta,
+            body
+        );
+    }
+
+    put(ruta: string, body: object) {
+        return this.httpClient.put(
+            GenericServicesService.API_ENDPOINT + ruta,
+            body
+        );
+    }
+
+    delete(ruta: string) {
+        return this.httpClient.delete(
+            GenericServicesService.API_ENDPOINT + ruta
+        );
+    }
+
+}
