36980bff90e4b500b265e3b917ddd3dabd96bf0a
Luis castañeda <gerard95@live.com.mx>
2019-05-30 10:02:44 -0500

    rutas

--- src/app/services/creditos.service.ts
+++ src/app/services/creditos.service.ts
@@ -1,192 +1,186 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Lineamiento } from '../views/creditos/interfaces/lineamiento';
 import { Actividad } from '../views/creditos/interfaces/actividad';
 import {UsuarioActividad } from '../views/creditos/interfaces/usuarioActividad';
 import { AsistenteActividad } from '../views/creditos/interfaces/asistenteActividad';
-import { AsistenciaAlumnoActividad } from '../views/creditos/interfaces/asistenciaAlumnoActividad'; 
+import { AsistenciaAlumnoActividad } from '../views/creditos/interfaces/asistenciaAlumnoActividad';
+import { GenericServicesService } from './generic-services.service';
 
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class CreditosService {
-API_ENDPOINT = "http://localhost:8000/api";
+export class CreditosService extends GenericServicesService{
 
-  constructor(private httpclient: HttpClient) { }
+  constructor(private httpclient: HttpClient,
+              private genericServicesService: GenericServicesService ) {  super(httpclient); }
+
+  //API_ENDPOINT = "http://localhost:8000/api";
+  private API_ENDPOINT = GenericServicesService.API_ENDPOINT;
+  private headers = GenericServicesService.HEADERS;
 
   get(){
-  	return this.httpclient.get(this.API_ENDPOINT + '/lineamientos');
+  	return this.httpclient.get(this.API_ENDPOINT + 'lineamientos');
   }
 
   save(lineamiento: Lineamiento){
-  	const Headers = new HttpHeaders({'Content-Type':'application/json'});
-  	console.log("en el servicio: " + lineamiento);
-  	return this.httpclient.post(this.API_ENDPOINT + '/lineamientos', lineamiento, {headers : Headers});
+  	return this.httpclient.post(this.API_ENDPOINT + 'lineamientos', lineamiento,  this.headers);
   }
 
   put(lineamiento){
-  const Headers = new HttpHeaders({'Content-Type':'application/json'});
-  return this.httpclient.put(this.API_ENDPOINT + '/lineamientos/' + lineamiento.PK_LINEAMIENTO, lineamiento, {headers : Headers});
-
+  return this.httpclient.put(this.API_ENDPOINT + 'lineamientos/' + lineamiento.PK_LINEAMIENTO, lineamiento,  this.headers);
+  Authorization
   }
 
   delete(id){
-    return this.httpclient.delete(this.API_ENDPOINT + '/lineamientos/' + id);
+    return this.httpclient.delete(this.API_ENDPOINT + 'lineamientos/' + id);
   }
 /*-------------------------------------Tipos--------------*/
 getTipos(){
-  return this.httpclient.get(this.API_ENDPOINT + '/tipos');
+  return this.httpclient.get(this.API_ENDPOINT + 'tipos');
 }
 /*-------------------------------------Actividades -------*/
 
   getActividades(){ /* para la gestion de las actividades, obtiene la vista actividades_v */
-    return this.httpclient.get(this.API_ENDPOINT + '/actividades');
+    return this.httpclient.get(this.API_ENDPOINT + 'actividades');
   }
 
   getActividadById(PK_ACTIVIDAD){
-    return this.httpclient.get(this.API_ENDPOINT + '/actividad-por-id/' + PK_ACTIVIDAD);
+    return this.httpclient.get(this.API_ENDPOINT + 'actividad-por-id/' + PK_ACTIVIDAD);
   }
 
   getActividadesRaw(){
-    return this.httpclient.get(this.API_ENDPOINT + '/actividades-raw');
+    return this.httpclient.get(this.API_ENDPOINT + 'actividades-raw');
   }
 
   getActividadesDisponibles(id){ /* Para los alumnos */
-    return this.httpclient.get(this.API_ENDPOINT + '/actividades-disponibles/' + id);
+    return this.httpclient.get(this.API_ENDPOINT + 'actividades-disponibles/' + id);
   }
 
   getActividadesInscitas(id){/* Para los alumnos */
-    return this.httpclient.get(this.API_ENDPOINT + '/actividades/' + id);
+    return this.httpclient.get(this.API_ENDPOINT + 'actividades/' + id);
   }
 
   saveActividades(actividad: Actividad){
-  	const Headers = new HttpHeaders({'Content-Type':'application/json'});
-  	return this.httpclient.post(this.API_ENDPOINT + '/actividades', actividad, {headers : Headers});
+  	return this.httpclient.post(this.API_ENDPOINT + 'actividades', actividad,  this.headers);
   }
   deleteActividades(id){
-    return this.httpclient.delete(this.API_ENDPOINT + '/actividades/' + id);
+    return this.httpclient.delete(this.API_ENDPOINT + 'actividades/' + id);
   }
   putActividades(actividad){
-    
-    const Headers = new HttpHeaders({'Content-Type':'application/json'});
-    return this.httpclient.put(this.API_ENDPOINT + '/actividades/' + actividad.PK_ACTIVIDAD, actividad, {headers : Headers});
+        return this.httpclient.put(this.API_ENDPOINT + 'actividades/' + actividad.PK_ACTIVIDAD, actividad,  this.headers);
   
     }
 
 /*-------------------------------------ALumno Actividades -------*/
 getRegistrados(PK_ACTIVIDAD){
   
-  return this.httpclient.get(this.API_ENDPOINT + '/alumno-actividades/' + PK_ACTIVIDAD);
+  return this.httpclient.get(this.API_ENDPOINT + 'alumno-actividades/' + PK_ACTIVIDAD);
 
 }
 
 saveAlumnoActividad(usuarioActividad: UsuarioActividad){
-  //console.log(usuarioActividad);
-  const Headers = new HttpHeaders({'Content-Type':'application/json'});
-  	return this.httpclient.post(this.API_ENDPOINT + '/alumno-actividades', usuarioActividad, {headers : Headers});
+  	return this.httpclient.post(this.API_ENDPOINT + 'alumno-actividades', usuarioActividad,  this.headers);
 
 }
 
 /* ----------------------------Asistencia ALumno Actividades ---------- */
 getListaActividades(FK_LINEAMIENTO, FK_ALUMNO){
-  return this.httpclient.get(this.API_ENDPOINT + '/lista-actividades/'+ FK_LINEAMIENTO + '/' + FK_ALUMNO);
+  return this.httpclient.get(this.API_ENDPOINT + 'lista-actividades/'+ FK_LINEAMIENTO + '/' + FK_ALUMNO);
 }
 
 
 /*-----------------------------ALumno Creditos -------------------------*/
 getCreditosbyAlumno(FK_ALUMNO){
-  return this.httpclient.get(this.API_ENDPOINT + '/alumno-creditos/' + FK_ALUMNO);
+  return this.httpclient.get(this.API_ENDPOINT + 'alumno-creditos/' + FK_ALUMNO);
 }
 
 /* ----------------------------Responsables de actividad --------------- */
 getResponsables(){//para mostrar a los responsables al mmomento de registrar una actividad
-  return this.httpclient.get(this.API_ENDPOINT + '/responsables-actividad');
+  return this.httpclient.get(this.API_ENDPOINT + 'responsables-actividad');
 }
 
 getActRes(FK_ALUMNO){//mostrar las actividades designadas a un reponsable en especifico
-  return this.httpclient.get(this.API_ENDPOINT + '/responsables-actividad/' + FK_ALUMNO);
+  return this.httpclient.get(this.API_ENDPOINT + 'responsables-actividad/' + FK_ALUMNO);
 }
 
 getListaAsistencia(PK_ACTIVIDAD){//mostrar la lista de asistentes a alguna actividad en específico
-  return this.httpclient.get(this.API_ENDPOINT + '/responsable-lista-asistentes/' + PK_ACTIVIDAD);
+  return this.httpclient.get(this.API_ENDPOINT + 'responsable-lista-asistentes/' + PK_ACTIVIDAD);
 
 }
 
 getAsistentes(PK_ACTIVIDAD){//mostrar los alumnos designados para tomar asistencia en una actividad
-  return this.httpclient.get(this.API_ENDPOINT + '/asistentes-actividad/' + PK_ACTIVIDAD);
+  return this.httpclient.get(this.API_ENDPOINT + 'asistentes-actividad/' + PK_ACTIVIDAD);
 }
 
 getAlumnoByNc(NUM_CONTROL){
-  return this.httpclient.get(this.API_ENDPOINT + '/alumnos-num-control/' + NUM_CONTROL);
+  return this.httpclient.get(this.API_ENDPOINT + 'alumnos-num-control/' + NUM_CONTROL);
 }
 
 getPkUsuario(NUM_CONTROL){
-  return this.httpclient.get(this.API_ENDPOINT + '/userid-num-control/' +  NUM_CONTROL);
+  return this.httpclient.get(this.API_ENDPOINT + 'userid-num-control/' +  NUM_CONTROL);
 }
 
 RegistrarAsistAct(asistenteActividad: AsistenteActividad){
-  //console.log(asistenteActividad);
-   const Headers = new HttpHeaders({'Content-Type':'application/json'});
-  return this.httpclient.post(this.API_ENDPOINT + '/asistentes-actividad', asistenteActividad, {headers : Headers}); 
+  return this.httpclient.post(this.API_ENDPOINT + 'asistentes-actividad', asistenteActividad,  this.headers); 
 }
 
 eliminarAsistAct(ID){
-  return this.httpclient.delete(this.API_ENDPOINT + '/asistentes-actividad/' + ID);
+  return this.httpclient.delete(this.API_ENDPOINT + 'asistentes-actividad/' + ID);
 }
     
 /*------------------------------- Alumnos con el rol para registrar asistencias -----------------------*/
 
 getActTomaAsist(PK_USUARIO){//Lista  de actividades en las que el alumno tomará asistencia
-  return this.httpclient.get(this.API_ENDPOINT + '/actividades-tomar-asistencia/' + PK_USUARIO);
+  return this.httpclient.get(this.API_ENDPOINT + 'actividades-tomar-asistencia/' + PK_USUARIO);
 }
 
 habilitarRegistro(PK_ACTIVIDAD){
-  return this.httpclient.get(this.API_ENDPOINT + '/registrar-asistencia/' + PK_ACTIVIDAD, {responseType: 'text'});
+  return this.httpclient.get(this.API_ENDPOINT + 'registrar-asistencia/' + PK_ACTIVIDAD, {responseType: 'text'});
 }
 
 eliminarAsistente(PK_USUARIO, PK_ACTIVIDAD){
-  return this.httpclient.get(this.API_ENDPOINT + '/eliminar-asistente-act/' + PK_USUARIO + '/' + PK_ACTIVIDAD);
+  return this.httpclient.get(this.API_ENDPOINT + 'eliminar-asistente-act/' + PK_USUARIO + '/' + PK_ACTIVIDAD);
 }
 
 eliminarRolAsistente(PK_USUARIO){
-  return this.httpclient.get(this.API_ENDPOINT + '/eliminar-rol-asistente/' + PK_USUARIO);
+  return this.httpclient.get(this.API_ENDPOINT + 'eliminar-rol-asistente/' + PK_USUARIO);
 }
 
 /*----------------------------- Servicios escolares -------------------------------------------------*/
 
 getCreditorPorValidar(){
-  return this.httpclient.get(this.API_ENDPOINT + '/creditos-por-validar');
+  return this.httpclient.get(this.API_ENDPOINT + 'creditos-por-validar');
 }
 
 getCreditosPorValidarByNumC(NUM_CONTROL){
-  return this.httpclient.get(this.API_ENDPOINT + '/creditos-por-validar-nc/' + NUM_CONTROL);
+  return this.httpclient.get(this.API_ENDPOINT + 'creditos-por-validar-nc/' + NUM_CONTROL);
 }
 getCreditosValidadosByNumC(NUM_CONTROL){
-  return this.httpclient.get(this.API_ENDPOINT + '/creditos-validados-nc/' + NUM_CONTROL);
+  return this.httpclient.get(this.API_ENDPOINT + 'creditos-validados-nc/' + NUM_CONTROL);
 }
 getCreditosPorValidarByLin(LINEAMIENTO){
-  return this.httpclient.get(this.API_ENDPOINT + '/creditos-por-validar-ln/' + LINEAMIENTO);
+  return this.httpclient.get(this.API_ENDPOINT + 'creditos-por-validar-ln/' + LINEAMIENTO);
 }
 getCreditosValidadosByLin(LINEAMIENTO){
-  return this.httpclient.get(this.API_ENDPOINT + '/creditos-validados-ln/' + LINEAMIENTO);
+  return this.httpclient.get(this.API_ENDPOINT + 'creditos-validados-ln/' + LINEAMIENTO);
 }
 
 getActByCredito(PK_ALUMNO_CREDITO){
-  return this.httpclient.get(this.API_ENDPOINT + '/actividades-credito-cumplidos/' + PK_ALUMNO_CREDITO);
+  return this.httpclient.get(this.API_ENDPOINT + 'actividades-credito-cumplidos/' + PK_ALUMNO_CREDITO);
 }
 
 validarCreditos(PK_ALUMNO_CREDITO){
-  const Headers = new HttpHeaders({'Content-Type':'application/json'});
-  return this.httpclient.put(this.API_ENDPOINT + '/validar-credito/' + PK_ALUMNO_CREDITO, {headers: Headers} );
+  return this.httpclient.put(this.API_ENDPOINT + 'validar-credito/' + PK_ALUMNO_CREDITO,  this.headers );
 }
 getCreditosValidados(){
-  return this.httpclient.get(this.API_ENDPOINT + '/creditos-validados');
+  return this.httpclient.get(this.API_ENDPOINT + 'creditos-validados');
 }
 
 generarConstancia(PK_ALUMNO_CREDITO){
-  return this.httpclient.get(this.API_ENDPOINT + '/generar-constancia/' + PK_ALUMNO_CREDITO);
+  return this.httpclient.get(this.API_ENDPOINT + 'generar-constancia/' + PK_ALUMNO_CREDITO);
 
 }
 /* 
@@ -199,8 +193,7 @@ verConstancia(PK_ALUMNO_CREDITO){
 /**registro de enttrada y salida actividades */
 
 registrarEntrada(asistenciaAlumnoActividad: AsistenciaAlumnoActividad){
-  const Headers = new HttpHeaders({'Content-Type':'application/json'});
-  return this.httpclient.post(this.API_ENDPOINT + '/asistencia-alumnos', asistenciaAlumnoActividad, {headers: Headers});
+  return this.httpclient.post(this.API_ENDPOINT + 'asistencia-alumnos', asistenciaAlumnoActividad,  this.headers);
 
 }
 
@@ -209,22 +202,22 @@ registrarEntrada(asistenciaAlumnoActividad: AsistenciaAlumnoActividad){
 /*-----------------ADMINISTRADOR DE USUARIOS --------- */
 singupAdmin(data){
   
-  return this.httpclient.post(this.API_ENDPOINT + '/signupAdminCC', data);
+  return this.httpclient.post(this.API_ENDPOINT + 'signupAdminCC', data);
 }
 
 getUsuarioByCurp(curp){
-  return this.httpclient.get(this.API_ENDPOINT + '/usuario-by-curp/' + curp);
+  return this.httpclient.get(this.API_ENDPOINT + 'usuario-by-curp/' + curp);
 }
 
 AgregarUsuarioCA(usuario){
- return this.httpclient.get(this.API_ENDPOINT + '/agregar-user-ca/' + usuario);
+ return this.httpclient.get(this.API_ENDPOINT + 'agregar-user-ca/' + usuario);
 }
 
 AgregarUsuarioJC(usuario){
-  return this.httpclient.get(this.API_ENDPOINT + '/agregar-user-jc/' + usuario);
+  return this.httpclient.get(this.API_ENDPOINT + 'agregar-user-jc/' + usuario);
  }
  AgregarUsuarioRA(usuario){
-  return this.httpclient.get(this.API_ENDPOINT + '/agregar-user-ra/' + usuario);
+  return this.httpclient.get(this.API_ENDPOINT + 'agregar-user-ra/' + usuario);
  }
  
 
