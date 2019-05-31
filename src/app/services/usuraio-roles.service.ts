a3d92c943b5da5016deca68f93f57ce8dfb3848f
Eduardo <eddyTEC>
2019-05-31 16:47:05 -0500

    Rutas jarwis

--- src/app/services/usuraio-roles.service.ts
+++ src/app/services/usuraio-roles.service.ts
@@ -5,6 +5,15 @@ import { IRoles } from './rolesLista';
 import { GenericServicesService } from './generic-services.service';
 
 
+/* @Injectable()
+export class UsuarioRolesService extends GenericServicesService{    
+    constructor(private http: HttpClient,
+    private genericServicesService: GenericServicesService ){ super(http); }
+    getUsuarioRoles(): Observable<IRoles[]>{
+        return this.http.get<IRoles[]>('http://127.0.0.1:8000/api/Usuario_Rol/' + sessionStorage.getItem('IdUsuario'));
+    }
+} */
+
 @Injectable()
 export class UsuarioRolesService extends GenericServicesService{    
     constructor(private http: HttpClient,
