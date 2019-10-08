dd8bbd408a4fcfb5e2327ba0819ff325b95c31a4
Miguel Peña <miguelangel.plopez@itleon.edu.mx>
2019-10-08 15:53:29 -0500

    Cambios de nombre en endpoint

--- src/app/services/generic-services.service.ts
+++ src/app/services/generic-services.service.ts
@@ -7,16 +7,16 @@ import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 export class GenericServicesService {
 
     // AMBIENTE DE PRODUCCIÓN
-    //protected static API_ENDPOINT = 'http://tecvirtual.itleon.edu.mx/backend_swiitl/server.php/api/';
-    //protected static ENDPOINT = 'http://tecvirtual.itleon.edu.mx/backend_swiitl/public/';
+    protected static API_ENDPOINT = 'http://tecvirtual.itleon.edu.mx/backend_swiitl/server.php/api/';
+    protected static ENDPOINT = 'http://tecvirtual.itleon.edu.mx/backend_swiitl/public/';
 
     // AMBIENTE DE PRUEBAS
     // protected static API_ENDPOINT = 'http://10.0.31.10/backend_swiitl/server.php/api/';
     // protected static ENDPOINT = 'http://10.0.31.10/backend_swiitl/server.php/';
 
     // AMBIENTE LOCAL
-    protected static API_ENDPOINT = 'http://127.0.0.1:8000/api/';
-    protected static ENDPOINT = 'http://127.0.0.1:8000/';
+    // protected static API_ENDPOINT = 'http://127.0.0.1:8000/api/';
+    // protected static ENDPOINT = 'http://127.0.0.1:8000/';
 
     protected static HEADERS = {
         headers: new HttpHeaders({
