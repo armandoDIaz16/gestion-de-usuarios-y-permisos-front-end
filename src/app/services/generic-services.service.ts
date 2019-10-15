e160317d88aaff6b665d662802051178e95f7e2a
FabricioITL <14240570@itleon.edu.mx>
2019-10-15 17:55:38 -0500

    PAAE to PRO v2

--- src/app/services/generic-services.service.ts
+++ src/app/services/generic-services.service.ts
@@ -11,8 +11,8 @@ export class GenericServicesService {
     // protected static ENDPOINT = 'http://tecvirtual.itleon.edu.mx/backend_swiitl/public/';
 
     // AMBIENTE DE PRUEBAS
-    protected static API_ENDPOINT = 'http://10.0.31.10/backend_swiitl/server.php/api/';
-    protected static ENDPOINT = 'http://10.0.31.10/backend_swiitl/server.php/';
+    //protected static API_ENDPOINT = 'http://10.0.31.10/backend_swiitl/server.php/api/';
+    //protected static ENDPOINT = 'http://10.0.31.10/backend_swiitl/server.php/';
 
     // AMBIENTE LOCAL
     protected static API_ENDPOINT = 'http://127.0.0.1:8000/api/';
