e38dce7d9f89b1cbef5b7e6b80c0197ea3285d94
Miguel Peña <miguelangel.plopez@itleon.edu.mx>
2019-10-25 17:22:32 -0500

    Ajuste en archivo de generación de menú

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -65,7 +65,6 @@ export class DefaultLayoutComponent implements OnDestroy, OnInit {
     }
 
     logout(event: MouseEvent) {
-        sessionStorage.clear();
         event.preventDefault();
         this.Token.remove();
         this.Auth.changeAuthStatus(false);
