fb2adff5b93dd2a1f48b8752a2c97a69423548bd
Miguel Angel Pérez Ramírez <grifonus745@gmail.com>
2019-11-03 17:24:18 -0600

    Correcciones creditos complementarios

--- src/app/views/creditos/interfaces/usuario.ts
+++ src/app/views/creditos/interfaces/usuario.ts
@@ -2,7 +2,7 @@ export interface Usuario {
     PK_USUARIO?: number;
     PRIMER_APELLIDO: string;
     SEGUNDO_APELLIDO: string;
-    name: string;
+    NOMBRE: string;
     ENTRADA?: string;
     SALIDA?: string;
 }
