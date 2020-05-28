81fb640d77f1e75b4b06b4a65337d0f7551db1a3
mangelplopez <mangel.plopez@hotmail.com>
2020-05-28 09:28:03 -0500

    Horario service de alumno

---
+++ src/app/services/tutorias/horario_alumno/horario-alumno.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {GenericServicesService} from '../../generic-services.service';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {InterfaceAlumno} from '../../../models/tutorias/AlumnoModel';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HorarioAlumnoService extends GenericServicesService {
+
+    constructor(private http: HttpClient) {
+        super(http);
+    }
+
+    get_horario(alumno: any): any {
+        return this.http.get<any>(
+            GenericServicesService.API_ENDPOINT + 'get_horario_alumno/' + alumno,
+            GenericServicesService.HEADERS
+        );
+    }
+
+}
